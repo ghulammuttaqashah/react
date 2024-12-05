b5b54f647ebc7be5d904d3efd8855a011e835718
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2024-12-05 16:37:44 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MyComponent3 from "./MyComponent3";
 //import List from "./List.jsx";
 //import UserGreeting from "./UserGreeting.jsx";
 //import MyComponent from "./MyComponent";
-import ColorPicker from "./ColorPicker";
+/*import ColorPicker from "./ColorPicker";
 import MyComponent2 from "./MyComponent2";
 import MyComponent4 from "./MyComponent4";
 import MyComponent5 from "./MyComponent5";
@@ -17,10 +17,16 @@ import MyComponent7 from "./MyComponent7";
 import DigitalClock from "./DigitalClock";
 import ComponentA from "./ComponentA";
 import MyComponent8 from "./MyComponent8";
-import StopWatch from "./StopWatch";
+import StopWatch from "./StopWatch";*/
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 function App() {
+
+  
+
   return (<>
-    <StopWatch/>
+    
+    <Counter/>
     
   </>);
 }
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,7 +2,7 @@ import React,{useState} from "react"
 
 function ToDoList()
 {
-    const [tasks, setTasks]=useState(["Eat","code"]);
+    const [tasks, setTasks]=useState([]);
     const[newTask,setNewTasks]=useState("");
     
     function handleInputChange(event)
